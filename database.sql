6a58d096e1627982322c717311fefd28aad6257e
Nick Allen <rutherford.t.b@gmail.com>
2021-10-16 10:43:33 -0500

    filled db with sample tasks

--- database.sql
+++ database.sql
@@ -9,4 +9,9 @@ CREATE TABLE "tasks" (
 
 INSERT INTO tasks ("description", "complete", "date_due")
 VALUES
-('Do the laundry', '
+('Do the laundry', FALSE, '10/21/21'),
+('Exercise', FALSE, '10/21/21'),
+('Feed the Dog', FALSE, '10/21/21'),
+('Do the laundry', FALSE, '10/21/21');
+
+SELECT * FROM tasks;
