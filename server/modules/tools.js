1fe96ac8020636409f7ab5c45119adce140aa840
Nick Allen <rutherford.t.b@gmail.com>
2021-10-16 15:55:54 -0500

    add secondary sort order by id behind the scenes

--- server/modules/tools.js
+++ server/modules/tools.js
@@ -2,13 +2,13 @@ const tools = {
     sortBySorter: function (crit){
         switch(crit){
             case 'Date &uarr;':
-                return '"date_due" ASC;'
+                return '"date_due" ASC, id;'
             case 'Date &darr;':
-                return '"date_due" DESC;'
+                return '"date_due" DESC, id;'
             case 'Complete':
-                return '"complete";';
+                return '"complete" DESC, id;';
             case 'Incomplete':
-                return '"complete" DESC;';
+                return '"complete" ASC, id;';
             default:
                 console.log('sorter error');
         }
