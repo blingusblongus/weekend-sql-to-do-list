96690746d495e97dd9df36e0d6d53a1e98c5a7d7
Nick Allen <rutherford.t.b@gmail.com>
2021-10-17 15:51:16 -0500

    fixed sorting bug

--- server/modules/tools.js
+++ server/modules/tools.js
@@ -2,9 +2,9 @@ const tools = {
     sortBySorter: function (crit){
         switch(crit){
             case 'Date &uarr;':
-                return '"date_due" ASC, id;'
-            case 'Date &darr;':
                 return '"date_due" DESC, id;'
+            case 'Date &darr;':
+                return '"date_due" ASC, id;'
             case 'Complete':
                 return '"complete" DESC, id;';
             case 'Incomplete':
